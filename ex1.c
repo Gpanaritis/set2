971aec6053f28cf000a5538332dc4d8a1881abc1
Gpanaritis <59030376+Gpanaritis@users.noreply.github.com>
2021-01-13 23:41:25 +0200

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -1,27 +1,29 @@
 #include<stdio.h>
+
 #define b 8
+
 int main()
 {
-int a[b],i,j,temp;
-for (i=0;i<b;i++)
+  int a[b],i,j,temp;
+  for (i=0;i<b;i++)
+    {
+      printf("give number %d\n",i);
+      scanf("%d",&a[i]);
+    }
+  for (i=0;i<b;i++)
   {
-    printf("give number %d\n",i);
-    scanf("%d",&a[i]);
+    for (j=b-1;j>=i+1;j--)
+    {
+      if (a[j]<a[j-1]) {temp=a[j]; a[j]=a[j-1];a[j-1]=temp;}
+    }
   }
-for (i=0;i<b;i++)
- {
-  for (j=b-1;j>=i+1;j--)
+  temp=0;
+  for (i=1;i<b;i++)
   {
-    if (a[j]<a[j-1]) {temp=a[j]; a[j]=a[j-1];a[j-1]=temp;}
+    if (a[i]==a[i-1]) temp++;
   }
- }
- temp=0;
-for (i=1;i<b;i++)
- {
-  if (a[i]==a[i-1]) temp++;
- }
-printf("The number of repeats is %d",temp);
-
-return 0;
+  printf("The number of repeats is %d",temp);
+  
+  return 0;
 }
 
